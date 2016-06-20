c898df2689c1bc657dc8815ffa6faf89e739b7f9
ning <punnipa9177@gmail.com>
2016-06-20 16:47:05 +0700

    Finish 20 june

--- app/src/main/java/neu/punnipa/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/punnipa/bookshop/SignUpActivity.java
@@ -2,12 +2,51 @@ package neu.punnipa.bookshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText nameEditText, userEditText, passwordEditText;
+    private String nameString, userString, passwordString;
+    private static final string urlPHP = "http://swiftcodingthai.com/neu/add_user_Punnipa.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+
+
+    } // Main Methd
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
+
+        } else {
+            //No Space
+            uplonadToServer();
+        }
+
+
+    } // clickSing
+
+    private void uplonadToServer() {
+
     }
-}
+
+} // Main Class
