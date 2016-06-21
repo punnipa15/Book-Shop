02b75e6b56f5d78830344c9404386552db0abcfe
ning <punnipa9177@gmail.com>
2016-06-21 11:21:05 +0700

    Design Login

--- app/src/main/java/neu/punnipa/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/punnipa/bookshop/SignUpActivity.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวแปร
     private EditText nameEditText, userEditText, passwordEditText;
     private String nameString, userString, passwordString;
-    private static final string urlPHP = "http://swiftcodingthai.com/neu/add_user_Punnipa.php";
+    private static final String urlPHP = "http://swiftcodingthai.com/neu/add_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +57,28 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void uplonadToServer() {
 
-    }
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+    } // uplad
 
 } // Main Class
