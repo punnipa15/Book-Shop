ba89572e85b575f288314387d4e44f48a9c1f9e1
ning <punnipa9177@gmail.com>
2016-06-21 14:29:32 +0700

    json

--- app/src/main/java/neu/punnipa/bookshop/MainActivity.java
+++ app/src/main/java/neu/punnipa/bookshop/MainActivity.java
@@ -1,17 +1,24 @@
 package neu.punnipa.bookshop;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
-
+    private static final String urlJSON = "http://swiftcodingthai.com/neu/get_user.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,46 @@ public class MainActivity extends AppCompatActivity {
 
     } // Main Method
 
+    //Create Inner Class
+    private class MySynchronize extends AsyncTask<Void, Void, String> {
+
+        private Context context;
+        private String urlString;
+
+        public MySynchronize(Context context, String urlString) {
+            this.context = context;
+            this.urlString = urlString;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlString).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                return null;
+            }
+
+
+        } // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("BookShopV1", "JSON ==> " + s);
+
+        } // onPost
+
+    } // Class
+
     public void clickSignIn(View View){
 
         //Get Value from Edit text
@@ -38,10 +85,21 @@ public class MainActivity extends AppCompatActivity {
             myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง คะ");
 
         } else {
+
+            searchUserAnPassword();
+
+
             //No Space
 
         }
     } // clickSignIn
+
+    private void searchUserAnPassword() {
+
+        MySynchronize mySynchronize = new MySynchronize(this, urlJSON);
+        mySynchronize.execute();
+
+    } // searchUser
 
 
     public void clickSignUpMain(View view) {
